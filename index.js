77c179a63d89f5118482da45ebed5a813226c088
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-15 04:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ function printBadges(names){
 }
 
 
+function coinToss(){
+  return Math.random() >= 0.5
+}
 
+function tailsNeverFails(){
+  let counter = 0;
+  while(coinToss()){
+    counter++
+  }
+  return 'You got ${counter} tails in a row!`; 
+}
 
 
